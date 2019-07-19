7fc14ec589bfbbea2f48079625996268aeb526c9
Evelin Aasna <e.aasna@dkfz-heidelberg.de>
2019-07-19 17:29:53 +0200

    grouping cells by batch, identity, type

--- seurat_clustering.R
+++ seurat_clustering.R
@@ -1,21 +1,73 @@
 library(Seurat)
 library(ggplot2)
+#library(plyr)
 
 load(file = paste0("/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/joined.RData"))
 
 # based on elbowplot
 nPCA = 15
 
-seu <- FindNeighbors(seu, dims = 1:nPCA)
+#seu <- FindNeighbors(seu, dims = 1:nPCA)
 
-seu <- RunUMAP(seu, dims = 1:nPCA)
-DimPlot( seu, reduction = "umap" )
+
+if (FALSE){
+  metadata = read.table("/icgc/dkfzlsdf/analysis/B210/data/mf/metadata.tsv", header = TRUE)
+  
+  # adding sample identity
+  seu$orig.sample = as.factor(substr(colnames(seu), 1, 1))
+  seu$orig.sample = revalue(seu$orig.sample, c("A"="1", "C"="1", "T"="1", "G"="1"))
+  
+  # adding individual identity
+  seu$orig.ident = revalue(seu$orig.sample, c("1"="07", "2"="10", "3"="18", "4"="20", "5"="07", "6"="07", "7"="20"))
+  
+  # adding type identity
+  seu$orig.type = revalue(seu$orig.sample, c("1"="f", "2"="f", "3"="f", "4"="f", "5"="f", "6"="c", "7"="c"))
+  
+  # renaming sample identity based on metadata
+  seu$orig.sample = revalue(seu$orig.sample, c("1"="07efM", "2"="10dfM", "3"="18bfM", "4"="20afM", "5"="07dfM", "6"="07dcM", "7"="20acM"))
+}
+
+
+
+#Look at these methods later:
+BoldTitle()
+FontSize()
+
+#seu <- RunUMAP(seu, dims = 1:nPCA)
+DimPlot( seu, reduction = "umap" ) + 
+  labs(title = "UMAP grouped by clustering results") +
+  theme(plot.title = element_text(size = 15))
 ggsave("/icgc/dkfzlsdf/analysis/B210/Evelin/plots/umap_joined.pdf")
 
-seu <- RunTSNE(seu, dims = 1:nPCA)
-DimPlot(seu, label = TRUE, reduction = "tsne") + NoLegend()
+#seu <- RunTSNE(seu, dims = 1:nPCA)
+DimPlot(seu, reduction = "tsne") + 
+  labs(title = "tSNE grouped by clustering results") +
+  theme(plot.title = element_text(size = 15))
 ggsave("/icgc/dkfzlsdf/analysis/B210/Evelin/plots/tsne_joined.pdf")
 
-seu <- FindClusters(seu, resolution = 1)
 
-save(seu, file = paste0("/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/joined.RData"))
+reds = c("tsne", "umap")
+labels = c("tSNE", "UMAP")
+for (i in range(1,2)){
+#if (FALSE){
+  DimPlot(seu, reduction = reds[i], group.by="orig.sample") + 
+    labs(title = paste(labels[i],"grouped by batch")) +
+    theme(plot.title = element_text(size = 15))
+  ggsave(paste0("/icgc/dkfzlsdf/analysis/B210/Evelin/plots/",reds[i],"_by_sample.pdf"))
+  
+  
+  DimPlot(seu, reduction = reds[i], group.by="orig.ident") + 
+    labs(title = paste(labels[i],"grouped by individual")) +
+    theme(plot.title = element_text(size = 15))
+  ggsave(paste0("/icgc/dkfzlsdf/analysis/B210/Evelin/plots/",reds[i],"_by_individual.pdf"))
+  
+  
+  DimPlot(seu, reduction = reds[i], group.by="orig.type") + 
+    labs(title = paste(labels[i],"grouped by type")) +
+    theme(plot.title = element_text(size = 15))
+  ggsave(paste0("/icgc/dkfzlsdf/analysis/B210/Evelin/plots/",reds[i],"_by_type.pdf"))
+}
+
+#seu <- FindClusters(seu, resolution = 1)
+
+#save(seu, file = paste0("/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/joined.RData"))
--- seurat_joined.R
+++ seurat_joined.R
@@ -1,11 +1,12 @@
 library(Seurat)
 library(ggplot2)
+library(devtools)
 
 dir_path = "/icgc/dkfzlsdf/analysis/B210/data/mf/"
 runs = c("cellranger201_count_23156_6_GRCh38", "cellranger201_count_23156_7_GRCh38", "cellranger201_count_23156_8_GRCh38", "cellranger201_count_24192-25608_4839STDY7131581_GRCh38", "cellranger201_count_24192-25608_4839STDY7131582_GRCh38", "cellranger201_count_24192-25608_4839STDY7131583_GRCh38", "cellranger201_count_24192-25608_4839STDY7131584_GRCh38")
 
 seu <- Read10X(paste0(dir_path, runs, "/outs/raw_gene_bc_matrices/GRCh38/"))
-seu <- CreateSeuratObject( seu, min.cells = 3 )
+seu <- CreateSeuratObject( seu, min.cells = 3)
 
 # Storing percentage of mitochondrial genes in object meta data
 seu <- PercentageFeatureSet(seu, pattern = "^MT-", col.name = "percent.mt")
@@ -15,7 +16,9 @@ seu = subset(seu, cells = high_mt_cells, invert = TRUE)
 
 
 # SCTransform could replace NormalizeData, FindVariableFeatures, ScaleData
-#seu <- SCTransform(seu, vars.to.regress = "percent.mt", verbose = FALSE)
+#seu <- SCTransform(seu, vars.to.regress = "percent.mt", verbose = TRUE, conserve.memory = TRUE)
+
+normalized_data <- sctransform::vst(seu[["RNA"]]@counts)$y
 seu <- NormalizeData(seu)
 seu <- FindVariableFeatures(seu, selection.method = "vst", nfeatures = 2000)
 
@@ -24,7 +27,7 @@ top10 <- head(VariableFeatures(seu), 10)
 
 # plot variable features with and without labels
 plot1 <- VariableFeaturePlot(seu)
-pdf("/icgc/dkfzlsdf/analysis/B210/Evelin/plots/most_variable_genes.pdf") 
+pdf("/icgc/dkfzlsdf/analysis/B210/Evelin/plots/most_variable_genes_SCT.pdf") 
 plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
 plot2
 dev.off()
@@ -35,8 +38,8 @@ dev.off()
 #x <- number of expressed genes
 n_detected_genes_sorted <- sort(seu$nFeature_RNA, decreasing = TRUE)
 
-pdf("/icgc/dkfzlsdf/analysis/B210/Evelin/plots/joined_kneeplot.pdf") 
+pdf("/icgc/dkfzlsdf/analysis/B210/Evelin/plots/joined_kneeplot_SCT.pdf") 
 plot(seq.int(length(n_detected_genes_sorted)), n_detected_genes_sorted, log = "xy", type = "s", xlab = "barcodes", ylab = "detected genes")
 dev.off()
 
-save(seu, file = paste0("/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/joined.RData"))
+save(seu, file = paste0("/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/joined_SCT.RData"))
