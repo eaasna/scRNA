a868d3c743b133597bcb9438bc77671a88080dec
Evelin Aasna <e.aasna@dkfz-heidelberg.de>
2019-07-24 15:41:29 +0200

    find overlap between known markergenes and analysis results

--- auroc.R
+++ auroc.R
@@ -1,6 +1,5 @@
-library(pROC)
 load(file = paste0("/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/joined.RData"))
-
+load("/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/goi.RData")
 
 load("/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/selPoints.RData")
 
@@ -17,32 +16,24 @@ DimPlot(seu, reduction = "umap", group.by="lasso") +
   theme(plot.title = element_text(size = 20))
 
 
-# row variance for column-sparse matrix
-# method from: https://www.zmbh.uni-heidelberg.de/anders/div/sc_pedestrian.html
-colVars_spm <- function( spm ) {
-  stopifnot( is( spm, "dgCMatrix" ) )
-  ans <- sapply( seq.int(spm@Dim[2]), function(j) {
-    mean <- sum( spm@x[ (spm@p[j]+1):spm@p[j+1] ] ) / spm@Dim[1]
-    sum( ( spm@x[ (spm@p[j]+1):spm@p[j+1] ] - mean )^2 ) +
-      mean^2 * ( spm@Dim[1] - ( spm@p[j+1] - spm@p[j] ) ) } ) / ( spm@Dim[1] - 1 )
-  names(ans) <- spm@Dimnames[[2]]
-  ans
+library(pROC)
+
+informative_genes = VariableFeatures(seu)
+selPoints_bool <-seq.int(ncol(seu[["RNA"]]@counts)) %in% selPoints
+# from https://www.zmbh.uni-heidelberg.de/anders/div/sc_pedestrian.html
+aucs <- sapply( informative_genes, function(g)
+  auc( roc( selPoints_bool, seu[["RNA"]]@counts[g,] ) ) )
+
+
+marker_genes = names( head( sort( aucs, decreasing = TRUE ), 20 ) )
+
+# print name of celltype and which marker genes overlap (if there is overlap)
+
+find_overlap<- function(known_markers){
+  if (length(which(marker_genes %in% known_markers)) > 0){
+    print(names(known_markers))
+    print(marker_genes[which(marker_genes %in% known_markers)])
+  }
 }
 
-rowVars_spm <- function( spm ) {
-  colVars_spm( t(spm) )
-}
-
-gene_means <- rowMeans( seu[["RNA"]]@counts )
-gene_vars <- rowVars_spm( seu[["RNA"]]@counts )
-
-plot( gene_means, gene_vars / gene_means,
-      log = "xy", cex = .3, col = adjustcolor("black", alpha=.3), 
-      xlab = "mean", ylab = "variance / mean" )
-
-poisson_vmr <- mean( 1 / colSums( seu[["RNA"]]@counts ) )
-
-abline( h = 1:3 * poisson_vmr, col="lightblue" )
-
-informative_genes = names( which( gene_vars / gene_means  >  3 * poisson_vmr ) )
-length(informative_genes)
+sapply(goi, function(known_markers) find_overlap)
