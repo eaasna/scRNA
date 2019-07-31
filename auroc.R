f4d485653ccc540df7265e25c90c461da9af6149
Evelin Aasna <e.aasna@dkfz-heidelberg.de>
2019-07-31 17:24:57 +0200

    labelling clusters

--- auroc.R
+++ auroc.R
@@ -1,3 +1,6 @@
+library(Seurat)
+library(pROC)
+
 load(file = paste0("/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/joined.RData"))
 load("/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/goi.RData")
 
@@ -11,29 +14,28 @@ seu$lasso[selPoints]=1
 selCells = colnames(seu[["RNA"]]@counts)[selPoints]
 
 
-DimPlot(seu, reduction = "umap", group.by="lasso") + 
-  labs(title = paste("Lasso selection")) +
-  theme(plot.title = element_text(size = 20))
-
-
-library(pROC)
-
 informative_genes = VariableFeatures(seu)
 selPoints_bool <-seq.int(ncol(seu[["RNA"]]@counts)) %in% selPoints
 # from https://www.zmbh.uni-heidelberg.de/anders/div/sc_pedestrian.html
 aucs <- sapply( informative_genes, function(g)
   auc( roc( selPoints_bool, seu[["RNA"]]@counts[g,] ) ) )
 
+marker_genes = names( head( sort( aucs, decreasing = TRUE ), 40 ) )
 
-marker_genes = names( head( sort( aucs, decreasing = TRUE ), 20 ) )
-
+cell_annotation = ""
+gene_annotation = ""
 # print name of celltype and which marker genes overlap (if there is overlap)
-
-find_overlap<- function(known_markers){
+for (i in 1:length(goi)){
+  known_markers = goi[i]
   if (length(which(marker_genes %in% known_markers)) > 0){
-    print(names(known_markers))
-    print(marker_genes[which(marker_genes %in% known_markers)])
+    cell_annotation = names(known_markers)
+    gene_annotation = paste(marker_genes[which(marker_genes %in% known_markers)])
+    print(cell_annotation)
+    print(gene_annotation)
   }
 }
 
-sapply(goi, function(known_markers) find_overlap)
+DimPlot(seu, reduction = "umap", group.by="lasso") + 
+  labs(title = paste0(cell_annotation, ": ", gene_annotation)) +
+  theme(plot.title = element_text(size = 10))
+ggsave("/icgc/dkfzlsdf/analysis/B210/Evelin/plots/lasso_4.pdf")
---
+++ marker_genes.R
@@ -0,0 +1,38 @@
+library(Seurat)
+library(dplyr)
+library(ggplot2)
+
+load(file = paste0("/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/joined.RData"))
+load("/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/goi.RData")
+
+all_markers = unlist(goi, use.names = F)
+variable_genes = VariableFeatures(seu)
+variable_markers = all_markers[which(all_markers %in% variable_genes)]
+celltypes = names(goi)
+
+
+
+celltype = "dissociation_effects"
+load("/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/plotted_celltypes.pdf")
+plotted_celltypes[length(plotted_celltypes)+1] = celltype
+celltypes = celltypes[which(!(celltypes %in% plotted_celltypes))]
+save(plotted_celltypes, file="/icgc/dkfzlsdf/analysis/B210/Evelin/seurat_object/plotted_celltypes.pdf")
+markers_to_plot = goi[[celltype]][which(goi[[celltype]] %in% variable_markers)]
+#markers_to_plot = goi[[celltype]]
+print(length(markers_to_plot))
+
+col = 1
+h = 5
+w = 5
+VlnPlot(seu, features = markers_to_plot, ncol = col, pt.size = 0.1) + 
+  labs(title = celltype) +
+  theme(plot.title = element_text(size = 15))
+ggsave(paste0("/icgc/dkfzlsdf/analysis/B210/Evelin/plots/vln_",celltype,".pdf"), height = h, width = w)
+
+FeaturePlot(seu, features = markers_to_plot, reduction = "umap", ncol = col, pt.size = 0.2) + 
+  labs(title = celltype) +
+  theme(plot.title = element_text(size = 15))
+ggsave(paste0("/icgc/dkfzlsdf/analysis/B210/Evelin/plots/umap_",celltype,".pdf"), height = h, width = w)
+
+
+
